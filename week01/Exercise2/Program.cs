834bd8bbc94198451f89a74fcf60d7d218264615
serdotte <rodrigo.serdotte@gmail.com>
2025-03-04 14:34:03 -0300

    ex 002 - stretch challenge

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,41 +1,69 @@
 using System;
 
-Console.Write("What is your grade percentage? ");
-string answer = Console.ReadLine();
-int grade = int.Parse(answer);
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
 
-string letter = "";
+        //verifia se a entrada é um número valido
+        if (!int.TryParse(answer, out int grade))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+            return;
+        }
 
+        string letter = "";
 
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
 
-if (grade >= 90)
-{
-    letter = "A";
-}
-else if (grade >= 80)
-{
-    letter = "B";
-}
-else if (grade >= 70)
-{
-    letter = "C";
-}
-else if (grade >= 60)
-{
-    letter = "D";
-}
-else
-{
-    letter = "F";
-}
+        string signal = "";
 
-Console.WriteLine($"Your grade is {letter}");
+        //evita aplicar sinais para nota 'F'
+        if (grade >= 60)
+        {
+            int remainder = grade % 10;
 
-if (grade >= 70)
-{
-    Console.WriteLine("Congratulations you passed!");
-}
-else
-{
-    Console.WriteLine("You failed! Better luck next time!");
+            if (remainder >= 7)
+            {
+                signal = "+";
+            }
+            else if (remainder <= 3)
+            {
+                signal = "-";
+            }
+            
+        }
+
+        Console.WriteLine($"Your grade is {letter}{signal}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed! Better luck next time!");
+        }
+    }
 }
