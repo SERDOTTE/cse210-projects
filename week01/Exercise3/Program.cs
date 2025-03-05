f32116adab002b7b3b064f318ec2e33e68404d7c
serdotte <rodrigo.serdotte@gmail.com>
2025-03-05 09:40:05 -0300

    ex 003 - final ajusts and strech challenges

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,51 @@
+//Rodrigo Serdotte Freitas
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // In case we determine the magic number
+        //Console.WriteLine("Magic number Game");
+        //Console.WriteLine("What is the magic number?");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        // In case we generate a random number
+
+        string playagain = "yes";
+        while (playagain == "yes")
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int userGuess = -1;
+            int count = 0; //Initialize the count variable
+            while (userGuess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                count++; //Increment the count variable
+
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine("You took " + count + " attempts to guess the number!");
+                    Console.WriteLine("Do you want to play again? (yes/no)");
+                    playagain = Console.ReadLine();
+                    if (playagain == "no")
+                    {
+                        Console.WriteLine("Goodbye!");
+                    }
+                }    
+            }
+        }
     }
 }
